0b2f612f35a60b0aef98ef49c38a29797f4d4495
beemt <m.tratnikov@gmail.com>
2020-04-07 16:40:52 +0200

    first commit

---
+++ assets/js/loader.js
@@ -0,0 +1,6 @@
+window.addEventListener("load",function(){
+  const loader = document.querySelector(".wraploader");
+  loader.className+="hidden";
+  const loaderk = document.querySelector(".spinlodad");
+  loaderk.className+="hidden";
+});
