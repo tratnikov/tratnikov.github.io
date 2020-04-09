bb274d67c6929d1f71e594b3291b5a991353925b
beemt <m.tratnikov@gmail.com>
2020-04-09 17:33:08 +0200

    optimised

--- assets/js/loader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load",function(){
-  const loader = document.querySelector(".wraploader");
-  loader.className+="hidden";
-  const loaderk = document.querySelector(".spinlodad");
-  loaderk.className+="hidden";
-});
